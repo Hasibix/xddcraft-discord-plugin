0c65bfc752aeae7509850891f60fa29a1e18cec8
Hasibix <73306991+Hasibix@users.noreply.github.com>
2021-09-19 09:27:31 +0000

    fixed class problem

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public final class XDDCraftDiscordPlugin extends JavaPlugin {
+public final class net.hasibix.xddcraft.discord extends JavaPlugin {
   private net.dv8tion.jda.api.JDA jda;
   private TextChannel chatChannel;
   
