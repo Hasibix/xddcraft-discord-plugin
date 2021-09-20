9b228ece48b145996df5ee639dbc0f884e140445
Hasibix <73306991+Hasibix@users.noreply.github.com>
2021-09-20 06:25:16 +0000

    fixed a lot things

--- src/net/hasibix/xddcraftdiscord.java
+++ src/net/hasibix/xddcraftdiscord.java
@@ -18,7 +18,7 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public final class net.hasibix.xddcraft.discord extends JavaPlugin {
+public final class xddcraftdiscord extends JavaPlugin {
   private net.dv8tion.jda.api.JDA jda;
   private TextChannel chatChannel;
   
@@ -57,11 +57,13 @@ public final class net.hasibix.xddcraft.discord extends JavaPlugin {
 
       jda.addEventListener(new DiscordListener());
       getServer().getPluginManager().registerEvents( new SpigotListener(), plugin: this );
+    chatChannel.sendMessage("**Server has been started!** ✅ ||@everyone||")
   }
 
   @Override
   public void onDisable() {
      if(jda != null) jda.shutdownNow();
+    chatChannel.sendMessage("**Server has been stopped** 🛑")
   }
 
 
